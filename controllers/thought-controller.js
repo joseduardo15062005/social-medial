6961fcd58235bb65f9d962156c3e80019437c707
joseduardo15062005 <joseduardo15062005@gmail.com>
2021-12-01 22:15:28 -0800

    add thought api functionality

---
+++ controllers/thought-controller.js
@@ -0,0 +1,85 @@
+const { User, Thought } = require("../models");
+
+const thoughtController = {
+  //Get Thought by ID
+  async getThoughtById({ params }, res) {
+    try {
+      const { thoughtId } = params;
+      const dbThought = await Thought.findById(thoughtId);
+      if (!dbThought) {
+        return res.status(404).json({ message: "Thought not found" });
+      }
+      res.json(dbThought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error getting thought" });
+    }
+  },
+  //POST Create Thought
+  async createThought({ params, body }, res) {
+    try {
+      const { userId } = params;
+      //create thought
+      const thought = await Thought.create(body);
+      console.log("New Thought", thought);
+
+      //find the user and add the thought to the user's thoughts array
+      const dbUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { $addToSet: { thoughts: thought._id } },
+        { new: true }
+      );
+      if (!dbUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+      res.json(dbUser);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error creating thought" });
+    }
+  },
+  //Update Thought
+  async updateThought({ params, body }, res) {
+    try {
+      const { thoughtId } = params;
+      const dbThought = await Thought.findOneAndUpdate(
+        { _id: thoughtId },
+        body,
+        { new: true }
+      );
+      if (!dbThought) {
+        return res.status(404).json({ message: "Thought not found" });
+      }
+      res.json(dbThought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error updating thought" });
+    }
+  },
+  //Delete Thought
+  async deleteThought({ params }, res) {
+    try {
+      const { thoughtId, userId } = params;
+      const dbThought = await Thought.findOneAndDelete({ _id: thoughtId });
+      if (!dbThought) {
+        return res.status(404).json({ message: "Thought not found" });
+      }
+      //TODO: remove thought from user's thoughts array
+      const dbUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { $pull: { thoughts: thoughtId } },
+        { new: true }
+      );
+      if (!dbUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      res.json(dbThought);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "Error deleting thought" });
+    }
+  },
+};
+
+module.exports = thoughtController;
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -3,13 +3,41 @@ const { User } = require("../models");
 const userController = {
   //GET all users
   async getUsers(req, res) {
-    const users = await User.find();
-    res.send(users);
+    try {
+      const users = await User.find({})
+        .populate({
+          path: "thoughts",
+          select: "-__v",
+        })
+        .select("-__v")
+        .sort({ _id: -1 });
+      res.send(users);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Error getting users",
+        error: err,
+      });
+    }
   },
   //GET user by id
   async getUserById(req, res) {
-    const user = await User.findById(req.params.id);
-    res.send(user);
+    try {
+      const user = await User.findById(req.params.id)
+        .populate({
+          path: "thoughts",
+          select: "-__v",
+        })
+        .select("-__v")
+        .sort({ _id: -1 });
+      res.send(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Error getting user",
+        error: err,
+      });
+    }
   },
   //POST new user
   async createUser({ body }, res) {
@@ -30,8 +58,19 @@ const userController = {
   },
   //DELETE user by id
   async deleteUser({ params }, res) {
-    const user = await User.findByIdAndDelete(params.id);
-    res.json(user);
+    try {
+      const user = await User.findByIdAndDelete(params.id);
+
+      //TODO: Delete thoughts associated with user
+
+      res.json(user);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Error deleting user",
+        error: err,
+      });
+    }
   },
 };
 
